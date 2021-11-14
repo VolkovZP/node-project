7bea006c5eb68be73136cda7b3144bdfac1da3e8
Volkov <mark.volkovgit@gmail.com>
2021-11-14 12:41:55 +0200

    repair cart

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const path = require('path');
 const coursesRouts = require('./routes/courses')
 const mongoose = require('mongoose')
 const session = require('express-session')
+const MongoStore = require('connect-mongodb-session')(session)
 const homeRouts = require('./routes/home')
 const addRouts = require('./routes/add')
 const cardRouts = require('./routes/card');
@@ -12,8 +13,10 @@ const ordersRouts = require('./routes/orders');
 const Handlebars = require('handlebars')
 const exphbs = require('express-handlebars')
 const { allowInsecurePrototypeAccess } = require('@handlebars/allow-prototype-access')
-const User = require('./models/User')
 const varMiddlewaer = require('./middleware/variable')
+const userMiddlewaer = require('./middleware/user')
+
+const MOGNO_URI = "mongodb+srv://tripatka:ayVXGpTIQewxXpuz@cluster0.udjgn.mongodb.net/shop"
 /*setting handlebars*/
 const hbs = exphbs.create({
     defaultLayout: 'main',
@@ -25,17 +28,11 @@ app.set('view engine', 'hbs')
 app.set('vews', 'views')
 /** */
 
-
-
-app.use(async (req, res, next) => {
-    try {
-        const user = await User.findById('618ff1e4fadec0b327ee98a4')
-        req.user = user
-        next()
-    } catch (e) {
-        console.log(e)
-    }
+const store = new MongoStore({
+    collection: 'sessions',
+    uri: MOGNO_URI
 })
+
 
 app.use(express.static(path.join(__dirname, 'public')))
 app.use(express.urlencoded({ extended: true }))
@@ -44,9 +41,11 @@ app.use(session({
     secret: 'somthing',
     resave: false,
     saveUninitialized: false,
+    store
 }))
 
 app.use(varMiddlewaer)
+app.use(userMiddlewaer)
 
 app.use('/courses', coursesRouts)
 app.use('/add', addRouts)
@@ -59,20 +58,11 @@ const PORT = process.env.PORT || 3000
 
 async function start() {
     try {
-        const url = "mongodb+srv://tripatka:ayVXGpTIQewxXpuz@cluster0.udjgn.mongodb.net/shop"
-        await mongoose.connect(url, { useNewUrlParser: true })
+        await mongoose.connect(MOGNO_URI
+            , { useNewUrlParser: true })
         app.listen(PORT, () => {
             console.log(`server is running on port ${PORT}`)
         })
-        const candidate = await User.findOne()
-        if (!candidate) {
-            const user = new User({
-                email: 'serfentos12@mail.ru',
-                name: "Mark",
-                cart: { items: [] }
-            })
-            await user.save()
-        }
     } catch (e) {
         console.log(e)
     }
--- routes/auth.js
+++ routes/auth.js
@@ -1,23 +1,30 @@
 const { Router } = require('express')
+const User = require('../models/User')
 const router = Router()
-
 
 router.get('/login', async (req, res) => {
     res.render('auth/login', {
-        title: 'login',
-        isLogit: true
+        title: 'Авторизация',
+        isLogin: true
     })
 })
+
 router.get('/logout', async (req, res) => {
     req.session.destroy(() => {
-        res.render('auth/login')
+        res.redirect('/auth/login#login')
     })
 })
 
-
 router.post('/login', async (req, res) => {
+    const user = await User.findById('618ff1e4fadec0b327ee98a4')
+    req.session.user = user
     req.session.isAuthenticated = true
-    res.redirect('/')
+    req.session.save(err => {
+        if (err) {
+            throw err
+        }
+        res.redirect('/')
+    })
 })
 
 module.exports = router
