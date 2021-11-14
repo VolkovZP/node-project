430fd8a57eafe2065618b84ae794961c242f7bb0
Volkov <mark.volkovgit@gmail.com>
2021-11-14 10:08:34 +0200

    save session in db

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const path = require('path');
 const coursesRouts = require('./routes/courses')
 const mongoose = require('mongoose')
 const session = require('express-session')
+const MongoStrore = require('connect-mongodb-session')(session)
 const homeRouts = require('./routes/home')
 const addRouts = require('./routes/add')
 const cardRouts = require('./routes/card');
@@ -14,6 +15,7 @@ const exphbs = require('express-handlebars')
 const { allowInsecurePrototypeAccess } = require('@handlebars/allow-prototype-access')
 const User = require('./models/User')
 const varMiddlewaer = require('./middleware/variable')
+const MONGODB_URI = "mongodb+srv://tripatka:ayVXGpTIQewxXpuz@cluster0.udjgn.mongodb.net/shop"
 /*setting handlebars*/
 const hbs = exphbs.create({
     defaultLayout: 'main',
@@ -25,6 +27,13 @@ app.set('view engine', 'hbs')
 app.set('vews', 'views')
 /** */
 
+
+const store = new MongoStrore({
+    collection: 'sessions',
+    uri: MONGODB_URI,
+})
+
+
 app.use(express.static(path.join(__dirname, 'public')))
 app.use(express.urlencoded({ extended: true }))
 
@@ -32,6 +41,7 @@ app.use(session({
     secret: 'somthing',
     resave: false,
     saveUninitialized: false,
+    store
 }))
 
 app.use(varMiddlewaer)
@@ -47,8 +57,7 @@ const PORT = process.env.PORT || 3000
 
 async function start() {
     try {
-        const url = "mongodb+srv://tripatka:ayVXGpTIQewxXpuz@cluster0.udjgn.mongodb.net/shop"
-        await mongoose.connect(url, { useNewUrlParser: true })
+        await mongoose.connect(MONGODB_URI, { useNewUrlParser: true })
         app.listen(PORT, () => {
             console.log(`server is running on port ${PORT}`)
         })
